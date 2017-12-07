4ecb2c19ff8399639b7232e35cf1137fb841c44a
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-12-07 11:38:13 -0600

    initial auth with token set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :items
   resources :favorites
   resources :guides
   resources :reviews
@@ -8,12 +9,12 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
 
-  get 'sessions/new'
+  #get 'sessions/new'
 
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-
+  #get 'signup', to: 'users#new', as: 'signup'
+  #get 'login', to: 'sessions#new', as: 'login'
+  #get 'logout', to: 'sessions#destroy', as: 'logout'
+  post 'authenticate', to: 'authentication#authenticate'
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205213524) do
+ActiveRecord::Schema.define(version: 20171207172052) do
 
   create_table "categories", force: :cascade do |t|
     t.string "section"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20171205213524) do
   create_table "guides", force: :cascade do |t|
     t.string "item"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -71,7 +78,7 @@ ActiveRecord::Schema.define(version: 20171205213524) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
