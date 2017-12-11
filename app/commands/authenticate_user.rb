7b6a1de083d433f7bba5d69b21162cbb47d3f6d4
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-12-11 10:21:26 -0600

    added lines to get back userid with token

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -1,24 +1,26 @@
-class AuthenticateUser
-  prepend SimpleCommand
+class AuthenticateUser 
+  prepend SimpleCommand 
+  def initialize(email, password) 
+    @email = email 
+    @password = password 
+  end 
 
-  def initialize(email, password)
-    @email = email
-    @password = password
-  end
+  def call 
+    token = JsonWebToken.encode(user_id: user.id) if user
+    user_id = user.id
+    token_with_user_id = {token: token, user_id: user_id}
+    return token_with_user_id
+  end 
 
-  def call
-    JsonWebToken.encode(user_id: user.id) if user
-  end
+  private 
 
-  private
+  attr_accessor :email, :password 
 
-  attr_accessor :email, :password
+  def user 
+    user = User.find_by_email(email) 
+    return user if user && user.authenticate(password) 
 
-  def user
-    user = User.find_by_email(email)
-    return user if user && user.authenticate(password)
-
-    errors.add :user_authentication, 'invalid credentials'
-    nil
-  end
+    errors.add :user_authentication, 'invalid credentials' 
+    nil 
+  end 
 end
